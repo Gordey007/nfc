48180504e91b21e74c332529c0c77c16ea68116a
Gordey007 <gordeyvasilev@gmail.com>
2018-09-23 17:34:29 +1000

    Nfc. Reads the key

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package gordey007.nfc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_log_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        btn_log_in = (Button) findViewById(R.id.btn_log_in);
+        btn_log_in.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_log_in:
+                Intent intent = new Intent(this, TimeActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
